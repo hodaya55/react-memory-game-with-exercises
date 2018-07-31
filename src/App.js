8fd8c9910ffa7c79e54f6b3593375f85ab8f8a81
hodaya55 <hodayah159@gmail.com>
2018-07-31 23:52:10 +0300

    disable new-game button

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
     else {
       // count ==0
       clearInterval(this.countDown);
+      this.setState({ isStartGame: false });
       this.gameOver();
     }
   }
