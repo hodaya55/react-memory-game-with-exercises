222467196a7ce4b5be13d9f3512af4618a8bfd97
batyahaim <batya307@gmail.com>
2018-08-02 19:11:05 +0300

    card work

---
+++ src/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import card from './card.png';
+
+
+const Card = (props) =>
+{
+  const handleClick = () => {
+    props.clickCard(pros.cardItem, props.index);
+  }
+
+return(
+  <div className={props.cardItem.flipped ? "card" : "card"} onClick={handleClick} >
+  {props.cardItem.flipped ? 
+  <img src={props.cardItem.src} alt="card-front" className="back" />
+   : <img src={card} alt="card-back" className="" /> }
+  </div>
+)
+
+}
+
+
+
+export default Card
+
+
+
+
