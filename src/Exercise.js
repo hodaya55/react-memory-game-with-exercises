7abc3551a0b75147d4234c1b9ad625ca34c32a6d
zohar <yeoodahed@gmail.com>
2018-08-02 19:04:41 +0300

    inputResulte

--- src/Exercise.js
+++ src/Exercise.js
@@ -6,6 +6,7 @@ import React, { Component } from 'react'
 class Exercise extends Component {
     constructor(props) {
         super(props);
+        this.result = 0;
         this.addExercise = this.addExercise.bind(this);
         this.calculateResulte = this.calculateResulte.bind(this);
     }
@@ -52,20 +53,31 @@ class Exercise extends Component {
         let operator = arrOperator[Math.floor(Math.random() * arrOperator.length)];
         //console.log("The operator: " + operator); //good
 
-        let resulte = this.calculateResulte(firstNum, secondNum, operator);
+        let resultExe = this.calculateResulte(firstNum, secondNum, operator);
         let theExerciseString = `${firstNum} ${operator} ${secondNum} = `;
-
-        console.log("The exercise: " + theExerciseString + '\nThe resulte: ' + resulte);
-        this.props.updateFunc(resulte, theExerciseString);
+        this.result = resultExe;
+        console.log("The exercise: " + theExerciseString + '\nThe resulte: ' + resultExe);
+        // this.props.updateFunc(resulte, theExerciseString);
+        return theExerciseString;
 
         //return `${firstNum} ${operator} ${secondNum} = `; //good
     }
+    checkRes = ({ target: { value } }) => {
+        if (value === this.result) {
+            this.props.updateFunc(true);
+        }
+        else
+            this.props.updateFunc(false);
 
+
+    }
 
     render() {
         return (
             <div className="exer">
                 {this.addExercise()}
+                {/* {this.exercise} */}
+                <input className="inputResulte" type="number" placeholder="?" onChange={this.checkRes} />
             </div>
         )
 
--- src/Exercises.js
+++ src/Exercises.js
@@ -7,19 +7,21 @@ import Exercise from './Exercise'
 class Exercises extends Component {
     constructor(props) {
         super(props);
-        this.state = { resulteExercise: 0, theExercise: "" };
+        this.state = { isCorrect: false };
         this.updateExercise = this.updateExercise.bind(this);
     }
 
-    updateExercise(theResulteCalc, exerciseString) {
-         //put the 'result' of the exercise in to the state & put the 'exercise' in to the state
-        this.setState({ resulteExercise: theResulteCalc, theExercise: exerciseString }); 
-        console.log(`this is the state :${this.state}`);
+    updateExercise(ans) {
+        //put the 'result' of the exercise in to the state & put the 'exercise' in to the state
+        // console.log(`this is the state :${this.state}`);
+
+        this.setState({ isCorrect: ans });
     }
 
     render() {
         return (
             <div className="exercises">
+                {/* {this.state.isCorrect ? } */}
                 <Exercise updateFunc={this.updateExercise} />
             </div>
         )
