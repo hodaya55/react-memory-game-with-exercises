9b775bdc33c032a42ddb1fafdbf4d830f5c4567c
hodaya55 <hodayah159@gmail.com>
2018-08-05 18:01:26 +0300

    update  cards num

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App timer={1} cardsNum={8} count={5} />, document.getElementById('root'));
+ReactDOM.render(<App timer={1} cardsNum={10} count={5} />, document.getElementById('root'));
 registerServiceWorker();
